966d9a9e261a9f086ee98d571dd6cd851bbfca5e
Drone <roxishan8@gmail.com>
2022-02-23 11:53:55 +0530

    keybord dismiss added

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,32 +1,50 @@
-import { Button, TextInput, View, StyleSheet, Text } from "react-native";
+import { Keyboard, Button, TouchableWithoutFeedback, View, StyleSheet, Text } from "react-native";
 import Card from '../components/Card';
 import Colors from "../constants/Colors";
+import Input from '../components/Input';
+import { useState } from "react";
 
 export default function StartGameScreen(props) {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = (inputText) => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    };
+
     return (
-        <View
-            style={styles.screen}
-        >
-            <Text style={styles.title}>Game Screen</Text>
-            <Card style={styles.inputContainer}>
-                <Text>
-                    Select a number
-                </Text>
-                <TextInput />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.buttons}>
-                        <Button color={Colors.accent} title="Reset" />
+        <TouchableWithoutFeedback onPress={
+            () => {
+                Keyboard.dismiss();
+            }
+        }>
+            <View
+                style={styles.screen}
+            >
+                <Text style={styles.title}>Game Screen</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>
+                        Select a number
+                    </Text>
+                    <Input
+                        onChangeText={numberInputHandler}
+                        value={enteredValue}
+                        keyboardType="numeric" style={styles.input} />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.buttons}>
+                            <Button color={Colors.accent} title="Reset" />
+                        </View>
+                        <View style={styles.buttons}>
+                            <Button color={Colors.primary} title="confirm" />
+                        </View>
                     </View>
-                    <View style={styles.buttons}>
-                        <Button color={Colors.primary} title="confirm" />
-                    </View>
-                </View>
-            </Card>
-        </View>
+                </Card>
+            </View></TouchableWithoutFeedback>
     );
 }
 
 const styles = StyleSheet.create({
+    input: { width: 160, textAlign: "center" },
     screen: {
         flex: 1,
         padding: 10,
